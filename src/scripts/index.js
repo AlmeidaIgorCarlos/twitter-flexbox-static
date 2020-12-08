d95ee8924b1c17cb0038d41c3ce330f0a8925936
almeidaigorcarlos <almeidaigorcarlos@gmail.com>
2020-12-08 11:46:03 -0300

    Commenting javascript files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,14 @@
+//Requiring css in order to be detected by webpack
 require('../style/main.css')
+
+//seach-subject lib
 const search_subject = require('./search-subject')
 
+//Getting all arrow images elements from DOM
 const arrowComponents = document.querySelectorAll('.first-line>img')
+
+//Getting the all hidden elements from DOM
 const floatingComponents = document.querySelectorAll('.first-line>ul')
+
+//Applying events to arrow and floating components
 search_subject.applyArrowImageClickEvent(arrowComponents, floatingComponents)
--- src/scripts/search-subject.js
+++ src/scripts/search-subject.js
@@ -1,9 +1,16 @@
+/**
+ * @function applyArrowImageClickEvent
+ * @param {Object[]} arrowComponents All arrow images recovered from DOM
+ * @param {Object[]} floatingComponents All hidden lists recovered from DOM
+ */
 module.exports.applyArrowImageClickEvent = function(arrowComponents, floatingComponents) {
+    //Validation of parameters
     if (typeof arrowComponents === 'undefined')
         throw new Error('Parameter "components" must not be undefined')
     if (arrowComponents === null)
         throw new Error('Parameter "components" must not be null')
 
+    //Applying event listener for each arrow components and its correspondent hidden list
     arrowComponents.forEach((component, index) => {
         component.addEventListener("click", () => {
             this.showFloatComponent(floatingComponents, index, true)
@@ -20,14 +27,23 @@ module.exports.applyArrowImageClickEvent = function(arrowComponents, floatingCom
     })
 }
 
+/**
+ * @function showFloatingComponent
+ * @param {Object[]} components List of components recovered from the DOM
+ * @param {Number} index Index from the list's element that will be shown or not
+ * @param {Boolean} showFloatingComponent Boolean value to determine if the component of the index will be shown or not
+ */
 module.exports.showFloatComponent = function(components, index, showFloatingComponent) {
+    //Validation of parameters
     if (components.length < 1)
-        throw new Error('there must be at least one component')
+    throw new Error('there must be at least one component')
     if (index < 0)
-        throw new Error('index must not be negative')
-
+    throw new Error('index must not be negative')
+    
+    //Component from the list by index
     const component = components[index]
-
+    
+    //Validation of parameters
     if (typeof component === 'undefined')
         throw new Error('ul component must be valid')
 
@@ -37,6 +53,7 @@ module.exports.showFloatComponent = function(components, index, showFloatingComp
     if (typeof component.style != 'object')
         throw new Error('Component property style must be an object')
 
+    //Ternary operator to decide if it's going to be shown or not
     component.style.display = showFloatingComponent === true ? 'inline-block' : 'none'
 
     return component
